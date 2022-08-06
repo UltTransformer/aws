a024a75859689ea97723b248ed751b85bf733799
UltTransformer <chris.g.noon@gmail.com>
2022-08-06 11:51:03 +0100

    adding subnet id values

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,3 +1,9 @@
 output "CN_VPC_1_ID" {
   value = aws_subnet.CN_Subnet_1.vpc_id
 }
+output "CN_Subnet_1_ID" {
+  value = aws_subnet.CN_Subnet_1.id
+}
+output "CN_Subnet_2_ID" {
+  value = aws_subnet.CN_Subnet_2.id
+}
