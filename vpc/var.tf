4788d71d78adb651b24e87fb8ed43e0ef5320e10
UltTransformer <chris.g.noon@gmail.com>
2022-08-06 10:33:44 +0100

    changing all CIDRs to vars

--- vpc/var.tf
+++ vpc/var.tf
@@ -4,3 +4,16 @@ variable "CN_VPC_1_CIDR" {
   type    = string
   default = "10.0.0.0/16"
 }
+variable "CN_Subnet_1" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+variable "CN_Subnet_2" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+variable "CN_Subnet_Public" {
+  type    = string
+  default = "10.0.101.0/24"
+}
+
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "CN_VPC_1" {
 # Subnet(s)
 resource "aws_subnet" "CN_Subnet_1" {
   vpc_id                  = aws_vpc.CN_VPC_1.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = var.CN_Subnet_1
   availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = false
   tags = {
@@ -19,7 +19,7 @@ resource "aws_subnet" "CN_Subnet_1" {
 }
 resource "aws_subnet" "CN_Subnet_2" {
   vpc_id                  = aws_vpc.CN_VPC_1.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = var.CN_Subnet_2
   availability_zone       = "eu-north-1b"
   map_public_ip_on_launch = false
   tags = {
@@ -28,7 +28,7 @@ resource "aws_subnet" "CN_Subnet_2" {
 }
 resource "aws_subnet" "CN_Subnet_Public" {
   vpc_id                  = aws_vpc.CN_VPC_1.id
-  cidr_block              = "10.0.101.0/24"
+  cidr_block              = var.CN_Subnet_Public
   availability_zone       = "eu-north-1b"
   map_public_ip_on_launch = true
   tags = {
