fe2f0538f9fbdf1a6d124780785f78c73de6b17c
UltTransformer <chris.g.noon@gmail.com>
2022-09-06 16:07:23 +0200

    remote state output pull testing

--- instances/remote_backend.tf
+++ instances/remote_backend.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "cn-env0-statefile-bucket"
-    key    = "env0c94f7d/vpc.tfstate"
+    key    = "vpc.terraform.tfstate"
     region = "eu-north-1"
   }
 }
