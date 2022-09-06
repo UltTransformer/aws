d014c86870618def99408599037c6d12650b476a
UltTransformer <chris.g.noon@gmail.com>
2022-09-06 12:11:26 +0200

    s3 acl update

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,7 +1,8 @@
 resource "aws_s3_bucket" "cn-env0-statefile-bucket" {
   bucket = "cn-env0-statefile-bucket"
-  aws_s3_bucket_acl    = "private"
-  tags = {
-    Name = "cn-env0-statefile-bucket"
-  }
+}
+
+resource "aws_s3_bucket_acl" "cn-env0-statefile-bucket" {
+  bucket = aws_s3_bucket.cn-env0-statefile-bucket.id
+  acl    = "private"
 }
