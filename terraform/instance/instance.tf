89bf2761dd200793ee7e9f3ea8b2b013d953371b
Chris Noon <chris.g.noon@gmail.com>
2022-09-14 11:57:48 +0200

    adding vars for env0

--- terraform/instance/instance.tf
+++ terraform/instance/instance.tf
@@ -2,6 +2,18 @@
 
 module "instance" {
   source              = "../../modules/instance"
-  ec2-instance-name   = "ec2-1"
-  security-group-name = "ec2-1-sg"
+  ec2-instance-name   = var.ec2-instance-name-local
+  security-group-name = var.security-group-name-local
+
+  variable "ec2-instance-name-local" {
+    description = "name of the s3 bucket. Must be unique."
+    type        = string
+    default     = "ec2-1"
+  }
+
+  variable "security-group-name-local" {
+    description = "name of the s3 bucket. Must be unique."
+    type        = string
+    default     = "ec2-1-sg"
+  }
 }
--- terraform/s3/s3.tf
+++ terraform/s3/s3.tf
@@ -2,10 +2,10 @@
 
 module "aws_s3_bucket" {
   source      = "../../modules/s3-bucket"
-  bucket_name = var.bucket_name_local
+  bucket_name = var.bucket-name-local
 }
 
-variable "bucket_name_local" {
+variable "bucket-name-local" {
   description = "name of the s3 bucket. Must be unique."
   type        = string
   default     = "cn-env0-statefile-bucket"
