5cb4bd0fe875e47a3ed75ed005ec9083f282d51f
Chris Noon <chris.g.noon@gmail.com>
2022-09-14 11:32:41 +0200

    terraform modules and terragrunt folders

--- terraform/s3/s3.tf
+++ terraform/s3/s3.tf
@@ -2,5 +2,10 @@
 
 module "aws_s3_bucket" {
   source      = "../../modules/s3-bucket"
-  bucket_name = "cn-env0-statefile-bucket"
+  bucket_name = var.bucket_name_local
+}
+
+variable "bucket_name_local" {
+  description = "name of the s3 bucket. Must be unique."
+  type        = string
 }
