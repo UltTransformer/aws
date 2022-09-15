95f687f6534458e426fd3948c193f50c9a96c81b
Chris Noon <chris.g.noon@gmail.com>
2022-09-15 11:28:15 +0200

    temp removing region as var

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -7,7 +7,7 @@ module "aws_s3_bucket" {
   subnet_1      = var.subnet_1_local
   subnet_2      = var.subnet_2_local
   subnet_public = var.subnet_public_local
-  region        = var.region_local
+  # region        = var.region_local
   sub_region_1  = var.sub_region_1_local
   sub_region_2  = var.sub_region_2_local
 }
@@ -30,10 +30,10 @@ variable "subnet_public_local" {
   type    = string
   default = "10.0.102.0/24"
 }
-variable "region_local" {
-  type    = string
-  default = "eu-north-1"
-}
+# variable "region_local" {
+#   type    = string
+#   default = "eu-north-1"
+# }
 variable "sub_region_1_local" {
   type    = string
   default = "eu-north-1a"
