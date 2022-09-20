5917ab0f84f083940917d2e0ad440848e0f4e785
UltTransformer <chris.g.noon@gmail.com>
2022-09-20 13:35:55 +0200

    removing subnet 2 from vpc module

--- modules/instance/import_data.tf
+++ modules/instance/import_data.tf
@@ -7,12 +7,12 @@ data "aws_subnet" "subnet_1" {
   }
 }
 
-data "aws_subnet" "subnet_2" {
-  filter {
-    name   = "tag:Name"
-    values = ["subnet_2"]
-  }
-}
+# data "aws_subnet" "subnet_2" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["subnet_2"]
+#   }
+# }
 
 data "aws_vpc" "vpc_1" {
   filter {
