8daa7e114708cc8b6ac501af39f6361b3e6069d9
UltTransformer <chris.g.noon@gmail.com>
2022-09-20 13:29:19 +0200

    removing subnet 2 from vpc module

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -17,15 +17,15 @@ resource "aws_subnet" "subnet_1" {
     Name = "subnet_1"
   }
 }
-resource "aws_subnet" "subnet_2" {
-  vpc_id                  = aws_vpc.vpc_1.id
-  cidr_block              = var.subnet_2
-  availability_zone       = var.sub_region_2
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "subnet_2"
-  }
-}
+# resource "aws_subnet" "subnet_2" {
+#   vpc_id                  = aws_vpc.vpc_1.id
+#   cidr_block              = var.subnet_2
+#   availability_zone       = var.sub_region_2
+#   map_public_ip_on_launch = false
+#   tags = {
+#     Name = "subnet_2"
+#   }
+# }
 resource "aws_subnet" "subnet_public" {
   vpc_id                  = aws_vpc.vpc_1.id
   cidr_block              = var.subnet_public
