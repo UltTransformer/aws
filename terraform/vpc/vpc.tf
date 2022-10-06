4f83bd50a615c03cd4d5220dfd99b0abba9856d5
UltTransformer <chris.g.noon@gmail.com>
2022-10-06 14:29:40 +0200

    removing new code

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -6,14 +6,14 @@ module "aws_vpc" {
   vpc_1_cidr      = var.vpc_1_cidr_local
   subnet_1        = var.subnet_1_local
   subnet_2        = var.subnet_2_local
-  subnet_3        = var.subnet_3_local  
+  # subnet_3        = var.subnet_3_local  
   subnet_public_1 = var.subnet_public_local_1
-  subnet_public_2 = var.subnet_public_local_2
-  subnet_public_3 = var.subnet_public_local_3
+  # subnet_public_2 = var.subnet_public_local_2
+  # subnet_public_3 = var.subnet_public_local_3
   region          = var.region_local
   sub_region_1    = var.sub_region_1_local
   sub_region_2    = var.sub_region_2_local
-  sub_region_3    = var.sub_region_3_local
+  # sub_region_3    = var.sub_region_3_local
 }
 
 # Vatiable definitions 
@@ -30,22 +30,22 @@ variable "subnet_2_local" {
   type    = string
   default = "10.0.2.0/24"
 }
-variable "subnet_3_local" {
-  type    = string
-  default = "10.0.3.0/24"
-}
+# variable "subnet_3_local" {
+#   type    = string
+#   default = "10.0.3.0/24"
+# }
 variable "subnet_public_local_1" {
   type    = string
   default = "10.0.101.0/24"
 }
-variable "subnet_public_local_2" {
-  type    = string
-  default = "10.0.102.0/24"
-}
-variable "subnet_public_local_3" {
-  type    = string
-  default = "10.0.103.0/24"
-}
+# variable "subnet_public_local_2" {
+#   type    = string
+#   default = "10.0.102.0/24"
+# }
+# variable "subnet_public_local_3" {
+#   type    = string
+#   default = "10.0.103.0/24"
+# }
 variable "region_local" {
   type    = string
   default = "eu-north-1"
@@ -58,7 +58,7 @@ variable "sub_region_2_local" {
   type    = string
   default = "eu-north-1b"
 }
-variable "sub_region_3_local" {
-  type    = string
-  default = "eu-north-1c"
-}
+# variable "sub_region_3_local" {
+#   type    = string
+#   default = "eu-north-1c"
+# }
