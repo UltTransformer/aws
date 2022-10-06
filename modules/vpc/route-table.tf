3c5150e64e02c41ad25abd61208a32c14193ef3b
UltTransformer <chris.g.noon@gmail.com>
2022-10-06 14:43:52 +0200

    adding new subnets to route tabler

--- modules/vpc/route-table.tf
+++ modules/vpc/route-table.tf
@@ -5,7 +5,6 @@ resource "aws_route_table" "route_table_private" {
     nat_gateway_id = aws_nat_gateway.nat_gw_1.id
   }
 }
-
 resource "aws_route_table" "route_table_public" {
   vpc_id = aws_vpc.vpc_1.id
   route {
@@ -13,18 +12,27 @@ resource "aws_route_table" "route_table_public" {
     gateway_id = aws_internet_gateway.igw.id
   }
 }
-
 resource "aws_route_table_association" "route_table_private_association_subnet_1" {
   subnet_id      = aws_subnet.subnet_1.id
   route_table_id = aws_route_table.route_table_private.id
 }
-
 resource "aws_route_table_association" "route_table_private_association_subnet_2" {
   subnet_id      = aws_subnet.subnet_2.id
   route_table_id = aws_route_table.route_table_private.id
 }
-
+resource "aws_route_table_association" "route_table_private_association_subnet_3" {
+  subnet_id      = aws_subnet.subnet_3.id
+  route_table_id = aws_route_table.route_table_private.id
+}
 resource "aws_route_table_association" "route_table_public_association_subnet_public_1" {
   subnet_id      = aws_subnet.subnet_public_1.id
+  route_table_id = aws_route_table.route_table_public.id
+}
+resource "aws_route_table_association" "route_table_public_association_subnet_public_2" {
+  subnet_id      = aws_subnet.subnet_public_2.id
+  route_table_id = aws_route_table.route_table_public.id
+}
+resource "aws_route_table_association" "route_table_public_association_subnet_public_3" {
+  subnet_id      = aws_subnet.subnet_public_3.id
   route_table_id = aws_route_table.route_table_public.id
 }
