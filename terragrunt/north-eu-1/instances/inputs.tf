ad392e0627ad25cdbb7e80a46d15106341f2b7e8
UltTransformer <chris.g.noon@gmail.com>
2022-10-13 12:19:42 +0200

    adding vars to TG code

--- terragrunt/north-eu-1/instances/inputs.tf
+++
@@ -1,9 +0,0 @@
-variable "ec2-web-instance-name-input" {
-  type = string
-  default = "ec2-1-web"
-}
-
-variable "ec2-web-security-group-name-input" {
-  type = string
-  default = "ec2-1-web-sg"
-}
