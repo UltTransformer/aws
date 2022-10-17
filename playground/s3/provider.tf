9656b1eea4708967b1ef64bba903cc543f707553
UltTransformer <chris.g.noon@gmail.com>
2022-10-17 12:37:56 +0200

    adding remote state

--- playground/s3/provider.tf
+++ playground/s3/provider.tf
@@ -9,3 +9,13 @@ terraform {
 provider "aws" {
   region = "eu-north-1"
 }
+
+# Remote backend configuration
+
+terraform {
+  backend "s3" {
+    bucket = "cn-env0-statefile-bucket"
+    key    = "s3-playground.tfstate"
+    region = "eu-north-1"
+  }
+}
