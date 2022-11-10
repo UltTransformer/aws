23162552e7d6bbfb4086ff8bc42b5cd6e87f13c3
UltTransformer <chris.g.noon@gmail.com>
2022-11-10 12:37:50 +0100

    updating basic TG examples

--- playground/customers/terragrunt/workflow-example/modules/subnets/subnets.tf
+++ playground/customers/terragrunt/workflow-example/modules/subnets/subnets.tf
@@ -1,6 +1,6 @@
 # subnet(s)
 resource "aws_subnet" "subnet_1" {
-  vpc_id                  = data.aws_vpc.vpc_1
+  vpc_id                  = data.aws_vpc.vpc_1.id
   cidr_block              = "10.10.10.0/24"
   availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = false
