0b379557afd68038197b139961fb850fb4558dda
UltTransformer <chris.g.noon@gmail.com>
2022-11-20 10:56:15 +0100

    sync playground

--- playground/s3/migrateStateFromTfcToLocal.sh
+++ playground/s3/migrateStateFromTfcToLocal.sh
@@ -3,11 +3,10 @@ terraform login
 terraform init
 terraform plan # Should run in TFC
 
-mkdir -p terraform.tfstate.d/tfc-migration-test
-terraform state pull > terraform.tfstate.d/tfc-migration-test/terraform.tfstate
+mkdir -p terraform.tfstate.d/tfc-migration
+terraform state pull > terraform.tfstate.d/tfc-migration/terraform.tfstate
 mv .terraform/terraform.tfstate .terraform/terraform.tfstate.old
-mv terraform.tfstate.d/tfc-migration-test/terraform.tfstate .terraform/terraform.tfstate
-mv .terraform/terraform.tfstate terraform.tfstate.d/tfc-migration-test/terraform.tfstate
+mv terraform.tfstate.d/tfc-migration/terraform.tfstate .terraform/terraform.tfstate
 # Remove the cloud block in the config
 terraform init
 terraform plan # Should run locally with no change
@@ -16,3 +15,8 @@ terraform login backend.api.env0.com
 # Add env0 cloud block
 terraform init -migrate-state -force-copy
 # Update env0 template configuration (TF version and VCS)
+
+
+
+
+mv .terraform/terraform.tfstate terraform.tfstate.d/tfc-migration/terraform.tfstate 
