d23d04422a3d589e3c53a185fae1b119c9f6ef38
UltTransformer <chris.g.noon@gmail.com>
2022-11-20 11:15:19 +0100

    adding s3 module playground

--- playground/s3-env0-module/s3.tf
+++ playground/s3-env0-module/s3.tf
@@ -2,7 +2,7 @@
 
 module "env0-module-s3" {
   source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/env0-module-s3/env0modules3"
-  version = "v1.0.0"
+  version = "1.0.0"
 }
 
 # Variable definition
