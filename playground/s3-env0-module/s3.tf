57b63cab779d91e41f9a384e4fd5d766d3fa85d1
UltTransformer <chris.g.noon@gmail.com>
2022-11-23 14:14:20 +0100

    updating module test code

--- playground/s3-env0-module/s3.tf
+++ playground/s3-env0-module/s3.tf
@@ -1,8 +1,8 @@
 # Call env0 module
 
-module "env0-module-s3" {
-  source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/env0-module-s3/env0modules3"
-  version = "v1.0.0"
+module "env0-s3-module" {
+  source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/env0-s3-module/env0s3module"
+  version = "1.0.0"
 }
 
 # Variable definition
