c9777ffee34049f4cf119bc289d08ec55384da4a
UltTransformer <chris.g.noon@gmail.com>
2022-11-24 14:54:46 +0100

    custom flow test

--- playground/s3-env0-module/s3.tf
+++ playground/s3-env0-module/s3.tf
@@ -1,6 +1,6 @@
 # Call env0 module
 
-module "s3bucket" {
-  source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/s3bucket/s3bucket"
+module "env0-module" {
+  source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/env0-module/env0module"
   version = "1.0.0"
 }
