409033dd2d4d2e1baecf9c140a67a297ccd37b88
UltTransformer <chris.g.noon@gmail.com>
2022-11-24 13:01:51 +0100

    custom flow test

--- playground/s3-env0-module/s3.tf
+++ playground/s3-env0-module/s3.tf
@@ -2,7 +2,6 @@
 
 module "env0-s3-module" {
   source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/env0-s3-module/env0s3module"
-# source      = "git::https://github.com/UltTransformer/aws.git//modules/s3-bucket"
   version = "1.0.0"
 }
 
