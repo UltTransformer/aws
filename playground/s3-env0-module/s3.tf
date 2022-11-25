f55773559ad034b414e17dea37f8330de29d18f3
UltTransformer <chris.g.noon@gmail.com>
2022-11-25 11:30:55 +0100

    updates to module code

--- playground/s3-env0-module/s3.tf
+++ playground/s3-env0-module/s3.tf
@@ -8,6 +8,6 @@ variable "bucket_name" {
 # Call module
 
 module "env0modules3" {
-  source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/env0modules3/env0modules3"
+  source = "api.env0.com/a25b6a59-9c48-4917-b082-68e985319a87/env0modules3/env0modules3//s3-bucket"
   version = "0.0.3"
 }
